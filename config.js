f3dfd3b11079a435a7eef9896b2a46887e892bd8
prodevil00 <105129222+prodevil00@users.noreply.github.com>
2023-04-16 13:55:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const Config = {
-    REDIRECTED_URL: "https://wissen-macht-den-unterschied.de/",
+    REDIRECTED_URL: "https://wissen-macht-den-unterschied.de",
     // CLIENT_URL:
     // SERVER_URL:
     GRAPHQL_ENDPOINT: "https://wissen-macht-den-unterschied.de/graphql",
